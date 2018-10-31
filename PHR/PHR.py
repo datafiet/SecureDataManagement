88b968890d5d41042bcdfbfa46fb5c9de7d46788
Yorick de Boer <mail@yorickdeboer.nl>
2018-10-31 17:13:44 +0100

    formatting

--- PHR/PHR.py
+++ PHR/PHR.py
@@ -23,23 +23,27 @@ Options:
 import sys
 from docopt import docopt
 from database import Database
+
 db = Database()
+
 
 def init():
     """ Initializes the database """
     db.initialize()
 
-def new_user(user,gender,date_of_birth,address,other):
+
+def new_user(user, gender, date_of_birth, address, other):
     """
     Create a new user, creates a Public Health Record and a key.
     :param user:    The new user to be created
     """
     key = "/path/to/key"
-    patient = [gender,date_of_birth,user,address,other]
+    patient = [gender, date_of_birth, user, address, other]
     patient
     print("Inserted new user in database at row {}".format(db.create_patient(patient)))
-    print("MOCK | new user \'{}\' created, key location: {}".format(user,key))
-    
+    print("MOCK | new user \'{}\' created, key location: {}".format(user, key))
+
+
 def login(user):
     """
     Logs a user into the system if the correct credentials are provided.
@@ -48,12 +52,13 @@ def login(user):
     if arguments['-k'] or arguments['--key']:
         key = arguments['<key>']
         # TODO: exit program if wrong key is provided
-        authenticate(user,key)
-        print("MOCK | {} logged in with key {}".format(user,key))
+        authenticate(user, key)
+        print("MOCK | {} logged in with key {}".format(user, key))
     else:
         sys.exit("Please provide a key with the \'-k\' or \'--key\' option for authentication.")
 
-def authenticate(user,key):
+
+def authenticate(user, key):
     """
     Authenticates a user, when authentication fails, the program should not continue.
     :param user:  User that needs to be authenticated
@@ -61,18 +66,21 @@ def authenticate(user,key):
     """
     print("MOCK | {} is authenticated".format(user))
 
-def read(user,key,record):
+
+def read(user, key, record):
     """
-    Function to read some public health record, this can only succeed if the user is allowed to read this. Returns all parts of the record that can be read.
+    Function to read some public health record, this can only succeed if the user is allowed to read this. Returns
+    all parts of the record that can be read.
     :param user:      User that wants to read the health record
     :param key:       Key of the user, used for decrypting the public health record
     :param record:    Public health record to be read
     """
-#    recordfile = get_record(record)
-#    decrypt(recordfile,key)
-    print("MOCK | Data is read from record \'{}\' by \'{}\'".format(record,user))
+    #    recordfile = get_record(record)
+    #    decrypt(recordfile,key)
+    print("MOCK | Data is read from record \'{}\' by \'{}\'".format(record, user))
 
-def insert(user,key,data,record):
+
+def insert(user, key, data, record):
     """
     Insert data into a public health record.
     :param user:    User that wants to insert data
@@ -82,10 +90,11 @@ def insert(user,key,data,record):
     """
     if record == None:
         record = user
-    print("MOCK | Data is inserted into record \'{}\' by \'{}\'".format(record,user))
+    print("MOCK | Data is inserted into record \'{}\' by \'{}\'".format(record, user))
     print("MOCK | Data inserted:\n{}".format(data))
-    
-def allow_access(user,key,to_user,data_type):
+
+
+def allow_access(user, key, to_user, data_type):
     """
     Allow another user read access to own public health record.
     :param user:        Owner of the public health record
@@ -93,24 +102,24 @@ def allow_access(user,key,to_user,data_type):
     :param to_user:     User that will get read access
     :param data_type:   Type of data that to_user will have access to
     """
-    print("MOCK | {} has provided {} with read access to their Public Health Record".format(user,to_user))
+    print("MOCK | {} has provided {} with read access to their Public Health Record".format(user, to_user))
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.1')
     if arguments['init']:
         init()
     if arguments['new'] and arguments['patient']:
-        new_user(arguments['<user>'],arguments['<gender>'],arguments['<date-of-birth>'],arguments['<patient-address>'],arguments['<other>'])
+        new_user(arguments['<user>'], arguments['<gender>'], arguments['<date-of-birth>'],
+                 arguments['<patient-address>'], arguments['<other>'])
     if arguments['-l'] or arguments['--login']:
         login(arguments['<user>'])
         if arguments['read']:
-            read(arguments['<user>'],arguments['<key>'],arguments['<record>'])
+            read(arguments['<user>'], arguments['<key>'], arguments['<record>'])
         if arguments['insert']:
-            insert(arguments['<user>'],arguments['<key>'],arguments['<data>'],arguments['<record>'])
+            insert(arguments['<user>'], arguments['<key>'], arguments['<data>'], arguments['<record>'])
         if arguments['allow-access']:
-            allow_access(arguments['<user>'],arguments['<key>'],arguments['<to-user>'],arguments['<type>'])
+            allow_access(arguments['<user>'], arguments['<key>'], arguments['<to-user>'], arguments['<type>'])
     else:
         print("Please provide login details.")
-    db.exit() # close database connection
-        
-        
+    db.exit()  # close database connection
